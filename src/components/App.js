9f4d2fc9b6ec74cc1a93786eb00a5119cfa9d2ef
sergey-sedykh <sedukhsergey@gmail.com>
2020-11-27 14:16:51 +0200

    update eslint rules

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-const a = 'asdfffffffffffffffffffffffffffffffffffffffffffffadfsssssddddddddddddddddddddddddddddddd'
-const App = ({title}) => {
-  return (
-    <div className={styles.app}>
-      {title}
-      <h2 className={'app'}>DDDDD</h2>
-    </div>
-  )
-};
+const App = ({ title }) => (
+  <div className={styles.app}>
+    {title}
+    <h2 className="app">DDDDD</h2>
+  </div>
+);
 
 export default App;
