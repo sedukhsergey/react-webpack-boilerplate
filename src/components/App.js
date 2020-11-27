1fd45925d2dca509f39a8e524e625f9ee8fa3d54
sergey-sedykh <sedukhsergey@gmail.com>
2020-11-27 14:09:57 +0200

    add eslint

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-
+const a = 'asdfffffffffffffffffffffffffffffffffffffffffffffadfsssssddddddddddddddddddddddddddddddd'
 const App = ({title}) => {
   return (
     <div className={styles.app}>
